262e99f8448642e13a0de1d7cd09b51ebf74ea88
John ORaw <49677015+JohnORaw@users.noreply.github.com>
2024-05-04 10:16:42 +0100

    Update first.sh

--- first.sh
+++ first.sh
@@ -2,6 +2,7 @@
 # Every RPi will be teated the same after boot.
 # All RPi will keep their configs etc. in GITHUB
 
+# Connect to Internet and do initial updates
 sudo apt update -y
 sudo apt upgrade -y 
 sudo apt install github
