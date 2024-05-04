279c69ffbd631e4d2a64817772927375d7fd30c7
John ORaw <49677015+JohnORaw@users.noreply.github.com>
2024-05-04 10:15:26 +0100

    Create first.sh

---
+++ first.sh
@@ -0,0 +1,7 @@
+# JOR: 04MAY24
+# Every RPi will be teated the same after boot.
+# All RPi will keep their configs etc. in GITHUB
+
+sudo apt update -y
+sudo apt upgrade -y 
+sudo apt install github
