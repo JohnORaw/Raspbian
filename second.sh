ddefa6e5f54d2592182360b6ae2620a25749d99f
John ORaw <49677015+JohnORaw@users.noreply.github.com>
2024-05-04 10:39:47 +0100

    Create second.sh

---
+++ second.sh
@@ -0,0 +1,10 @@
+# Get this repo as a test
+git clone git@github.com:JohnORaw/Raspbian.git
+cd Raspbian
+
+git status
+git config --global user.name "John ORaw"
+git config --global user.email "john.oraw@hotmail.com"
+git config --list
+echo '*************************************************'
+echo 'Hey' $USER ', all done!'
